213d84daf4ff50cb1ec993d0711447c700d36806
AP <ashis.n.patel@gmail.com>
2021-08-19 19:26:14 -0500

    Add player methods via prototypes to the player constructor

---
+++ __tests__/Player.test.js
@@ -0,0 +1,42 @@
+
+const { expect, test } = require('@jest/globals');
+const Player = require('../lib/Player'); 
+const Potion = require('../lib/Potion'); 
+
+jest.mock('../lib/Potion'); 
+
+console.log(new Potion()); 
+
+
+test('creates a player object', () => {
+    const player = new Player('Ashis'); 
+
+    expect(player.name).toBe('Ashis');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number)); 
+    expect(player.agility).toEqual(expect.any(Number)); 
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+
+}); 
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Ashis');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility'); 
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Ashis');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false); 
+});
